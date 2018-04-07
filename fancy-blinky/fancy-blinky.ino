1d9a711dcf60a6db666c00e46a36c282191e1afd
jsfleming <josofl12@gmail.com>
2018-04-07 18:45:02 -0400

    fancy-blinky: modified delay times
    Changed delay times from 1000ms to 100ms to allow for the change in
    brightness to be more apparent.

--- fancy-blinky/fancy-blinky.ino
+++ fancy-blinky/fancy-blinky.ino
@@ -6,21 +6,21 @@ void setup() {
 
 void loop() {
   analogWrite(ledPin, 0);     // set LED off
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 31);    // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 63);    // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 95);    // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 127);   // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 159);   // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 191);   // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 223);   // increment LED brightness
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
   analogWrite(ledPin, 255);   // set LED to brightest
-  delay(1000);                // wait 1 second
+  delay(100);                // wait 1 second
 }
